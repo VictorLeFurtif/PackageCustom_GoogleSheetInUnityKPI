5ea3b258684427d260d5558b89936d7614732696
BAZ Victor <v.baz@rubika-edu.com>
2025-12-15 15:20:15 +0100

    Helpers

---
+++ PackageKpiSheet/Assets/com.Victor.Utilities/Helpers.cs
@@ -0,0 +1,9 @@
+public static class Helpers
+    {
+        public static void ResetTransform(this Transform t)
+        {
+            t.localPosition = Vector3.zero;
+            t.localEulerAngles = Vector3.zero;
+            t.localScale = Vector3.one;
+        }
+}
